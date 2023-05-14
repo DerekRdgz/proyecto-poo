7303c5d8b05638eb0f603892e2bf8fe150da78db
DerekRdgz <derek_rdgz@outlook.com>
2023-05-14 14:00:05 -0600

    Arreglado Tipo de gasto de == a .equals

--- src/ConexionDB/ConexionBase.java
+++ src/ConexionDB/ConexionBase.java
@@ -21,7 +21,7 @@ public class ConexionBase {
     ResultSet resultado = null;
     
     public ConexionBase(){
-        String url = "jdbc:ucanaccess://C:\\Users\\perry\\Documents\\NetBeansProjects\\JavaAcces";
+        String url = "jdbc:ucanaccess://C:/Users/perry/Downloads/Programita/PERSONAL1.accdb";
         try{
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             conectar = DriverManager.getConnection(url);
